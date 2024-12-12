4722b94dabe6fbbc17b04d45db90f4ae9a160cee
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2024-12-11 22:54:26 -0600

    DD

---
+++ tools/lrcd-ref-dlg.tcl
@@ -0,0 +1,18 @@
+#|-lrcd-ref-dlg.tcl :
+#|  -declares procedures to calculate lrcd and other parameters associated 
+#|   _ to docking calculations .
+#|  -requires library lrcd.tcl v. 1.1.3, and userInfo v. 0.0.6 .
+#|  -requires preferentially
+#|  -author :-Carlos Z. Gómez Castro ;
+#|  -date :-2024-12-11.wed ;
+#|  -version information :
+#|    -version :-001 ;
+#|    -changes in current version :
+#|      -initial definitions as functions an procedures ;
+#|    -previous changes :- ;;
+#|  - ;
+
+
+
+
+#|- ;
