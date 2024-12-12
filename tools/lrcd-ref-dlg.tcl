4f6ff1eb237ee1d2544086f90ea11fa95a9dec81
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2024-12-11 23:34:00 -0600

    minor changes

--- tools/lrcd-ref-dlg.tcl
+++ tools/lrcd-ref-dlg.tcl
@@ -1,8 +1,10 @@
 #|-lrcd-ref-dlg.tcl :
 #|  -declares procedures to calculate lrcd and other parameters associated 
 #|   _ to docking calculations .
+#|  -is intended to create an output .csv file with energy, distCOM, and lrcd 
+#|   _ for all the conformations in a group of .dlg files with a common ref .
 #|  -requires library lrcd.tcl v. 1.1.3, and userInfo v. 0.0.6 .
-#|  -requires preferentially
+#|  -requires preferentially a molInfo script already processed .
 #|  -author :-Carlos Z. Gómez Castro ;
 #|  -date :-2024-12-11.wed ;
 #|  -version information :
