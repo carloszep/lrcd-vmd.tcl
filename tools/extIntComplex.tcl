b13b4161ca59eba0deb09f0e83effd35a3bc9a82
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2025-01-13 19:54:50 -0600

    minor changes

---
+++ tools/extIntComplex.tcl
@@ -0,0 +1,17 @@
+#|-extIntComplex.tcl :
+#|  -generate simplified versions of ligand-protein complexes .
+#|  -generates inputs for psfgen and namd .
+#|  - ;
+
+#|  -proc extIntComplex {id } :
+#|    -writes pdb files for each fragment in the protein that interacts with
+#|     _ the ligand .
+#|    -creates lists of residues to be mutated .
+#|    -notes :
+#|      -for the moment, both the ligand and the receptor most be in the same
+#|       _ molecule ;;
+proc extIntComplex {id } {
+  }
+
+#|- ;
+
